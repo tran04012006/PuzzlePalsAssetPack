d763767ba2fe8f6d1ce26f03c3f3f6907c11091d
Sarah <bichtranngo416@gmail.com>
2026-09-12 14:01:43 +0700

    update

--- Assets/Script/BlockyController.cs
+++ Assets/Script/BlockyController.cs
@@ -1,8 +1,18 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BlockyController : MonoBehaviour
 {
+    /*
+     * status = 0: idle
+     * status = 1: walk
+     * status = 2: receive block idle
+     * status = 3: receive block run
+     * status = 4: throw
+     */
+    [Header("Action")] public Action ReceiveBlockAction;
+    
     [Header("Component")]
     public Animator _animator;
     public SpriteRenderer spriteRenderer;
@@ -19,7 +29,11 @@ public class BlockyController : MonoBehaviour
     private IPlayerStatePattern currentState;
     public PlayerIdle _playerIdle;
     public PlayerWalk playerWalk;
-    public PlayerReceiveBlocks playerReceiveBlocks;
+    public PlayerReceiveBlocksIdle playerReceiveBlocks;
+    public PlayerReceiveBlockRun playerReceiveBlockRun;
+    public PlayerThrow playerThrow;
+
+    [Header("Receive block")] public GameObject receiveBlock;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,7 +47,9 @@ public class BlockyController : MonoBehaviour
         
         _playerIdle = new PlayerIdle(this);
         playerWalk = new PlayerWalk(this);
-        playerReceiveBlocks = new PlayerReceiveBlocks(this);
+        playerReceiveBlocks = new PlayerReceiveBlocksIdle(this);
+        playerReceiveBlockRun = new PlayerReceiveBlockRun(this);
+        playerThrow = new PlayerThrow(this);
 
         ChangeState(_playerIdle);
     }
@@ -43,10 +59,16 @@ public class BlockyController : MonoBehaviour
     {
         Movement();
         Direction();
-        //Debug.Log("currentState: !!!!!!!!!!!!!! " + currentState);
-        Debug.Log("moveY: !!!!!!!!!!!!!! " + moveY);
-
+        Debug.Log("currentState: !!!!!!!!!!!!!! " + currentState);
+        //Debug.Log("moveY: !!!!!!!!!!!!!! " + moveY);
+        rigidbody2D.linearVelocity = new Vector2(1, 0) * (moveX * moveSpeed);
         currentState.Update();
+        
+        if (receiveBlock != null && currentState == playerReceiveBlocks)
+        {
+            ReceiveBlockAction?.Invoke();
+            //BirdController
+        }
     }
 
     public void ChangeState(IPlayerStatePattern newState)
@@ -78,5 +100,14 @@ public class BlockyController : MonoBehaviour
             isRight = false;
             spriteRenderer.flipX = true;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Block"))
+        {
+            Debug.Log("Dung vao block !!!!!!!!!!!!");
+            receiveBlock = other.gameObject;
+        }    
     }
 }
